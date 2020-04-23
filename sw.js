4c542d20f913351e4a34532edbc328a01d496b07
xuwenjun <156845109@qq.com>
2020-04-23 23:13:02 +0800

    pic added

--- sw.js
+++ sw.js
@@ -1,21 +1,28 @@
 'use strict';
-
-self.addEventListener('fetch',function(event){
-	if(/\.jpg$|\.png$/.test(event.request.url)){
-		// if(event.request.headers.get('save-data')){
-		// 	alert('save-data');
-		// }
-		var supportsWebp = false;
-		if(event.request.headers.has('accept')){
-			supportsWebp = event.request.headers.get('accept').includes('webp');
-		}
-		if(supportsWebp){
-			console.log('支持webp');
-			var req = event.request.clone();
-			var returnUrl = req.url.substr(0,req.url.lastIndexOf('.')) + '.webp';
-			event.respondWith(
-				fetch(returnUrl,{mode: 'no-cors'})
-			)
-		}
-	}
+var cacheName = 'pwa-test01';
+self.addEventListener('install',function(event){
+	self.skipWaiting();
+	event.waitUntil(
+		caches.open(cacheName)
+		.then(cache => cache.addAll([
+			'./index.css',
+			'./second.html',
+			'./index.html'
+		]))
+	)
 })
+// self.addEventListener('fetch',function(event){
+// 	if(/\.jpg$|\.png$/.test(event.request.url)){
+// 		var supportsWebp = false;
+// 		if(event.request.headers.has('accept')){
+// 			supportsWebp = event.request.headers.get('accept').includes('webp');
+// 		}
+// 		if(supportsWebp){
+// 			var req = event.request.clone();
+// 			var returnUrl = req.url.substr(0,req.url.lastIndexOf('.')) + '.webp';
+// 			event.respondWith(
+// 				fetch(returnUrl,{mode: 'no-cors'})
+// 			)
+// 		}
+// 	}
+// })
